a80b1d39ed7b54aa77be66187db20858f2e10c59
tottiliyt <tottiliyt@gmail.com>
2022-04-25 13:23:23 -0400

    add test

--- src/test/java/GameGUITest.java
+++ src/test/java/GameGUITest.java
@@ -1,8 +1,7 @@
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,15 +9,12 @@ import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.api.FxRobot;
 import sample.Level;
-import sample.Login;
 
 import static org.testfx.api.FxAssert.verifyThat;
 import static org.testfx.matcher.base.NodeMatchers.isInvisible;
 import static org.testfx.matcher.base.NodeMatchers.isVisible;
-import static org.testfx.matcher.control.LabeledMatchers.hasText;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 @ExtendWith(ApplicationExtension.class)
@@ -26,15 +22,15 @@ public class GameGUITest {
 
     Stage bigStage;
     Level l;
-    int maxX;
-    int maxY;
+    int maxX = 950;
+    int maxY = 600;
 
     @Start
     void onStart(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/Game.fxml"));
 
         Parent root = loader.load();
-
+        stage.setScene(new Scene(root,950,600));
         stage.show();
         l=loader.getController();
         l.set_window(stage);
@@ -42,6 +38,8 @@ public class GameGUITest {
         l.set_root(root);
         l.startGame();
     }
+
+
 
     @Test
     void testHasLawn() {
@@ -71,26 +69,41 @@ public class GameGUITest {
     @Test
     void testPeashooter_lock(FxRobot robot) {
         verifyThat("#peashooter_lock", isInvisible());
-        robot.sleep(10, TimeUnit.SECONDS);
+        robot.sleep(5, TimeUnit.SECONDS);
         collectSun(robot);
         robot.clickOn("#peashooter_menu");
-        robot.clickOn(200,200);
+        robot.clickOn(300,200);
         verifyThat("#peashooter_lock", isVisible());
     }
 
     @Test
     void testSunflower_lock(FxRobot robot) {
         verifyThat("#sunflower_lock", isInvisible());
+        robot.sleep(5, TimeUnit.SECONDS);
+        collectSun(robot);
+        robot.clickOn("#sunflower_menu");
+        robot.clickOn(300,200);
+        verifyThat("#sunflower_lock", isVisible());
     }
 
     @Test
     void testWalnut_lock(FxRobot robot) {
         verifyThat("#walnut_lock", isInvisible());
+        robot.sleep(5, TimeUnit.SECONDS);
+        collectSun(robot);
+        robot.clickOn("#walnut_menu");
+        robot.clickOn(300,200);
+        verifyThat("#walnut_lock", isVisible());
     }
 
     @Test
     void testCherrybomb_lock(FxRobot robot) {
         verifyThat("#cherrybomb_lock", isInvisible());
+        robot.sleep(10, TimeUnit.SECONDS);
+        collectSun(robot);
+        robot.clickOn("#cherrybomb_menu");
+        robot.clickOn(300,200);
+        verifyThat("#cherrybomb_lock", isVisible());
     }
 
     @Test
@@ -102,26 +115,31 @@ public class GameGUITest {
     }
 
     @Test
-    void testSunCounter() {
+    void testSunCounter(FxRobot robot) {
         verifyThat("#sun_counter", isVisible());
-        verifyThat("#sun_counter", hasText("0"));
+        collectSun(robot);
+        verifyThat("#sun_counter", (TextField t) -> Integer.parseInt(t.getText()) > 0);
 
     }
 
     @Test
-    void testBar_zombie() {
+    void testMenu(FxRobot robot) {
+        verifyThat("#menu_pane", isInvisible());
+        robot.clickOn("#menu_button");
+        verifyThat("#menu_pane", isVisible());
+        robot.clickOn("#resume");
+        verifyThat("#menu_pane", isInvisible());
+        robot.clickOn("#menu_button");
+        robot.clickOn("#exit_button");
 
-    }
-
-    @Test
-    void testMenu() {
 
     }
 
     private void collectSun(FxRobot robot) {
-        for (int i = 0; i < 1000; i+=20) {
-            for (int j = 0; j < 1000; j+=20) {
+        for (int i = 300; i < maxX; i+=50) {
+            for (int j = 100; j < maxY; j+=50) {
                 robot.clickOn(i,j);
+
             }
         }
     }
